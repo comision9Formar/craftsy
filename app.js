2ec14c44e82b59a40005702be541c0b56e98d7d7
EricFormar <53233372+EricM76@users.noreply.github.com>
2021-07-14 17:26:32 -0300

    finish

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ app.get('/',(req,res) => res.sendFile(path.join(__dirname,'views','index.html'))
 
 app.get('/register',(req,res) => res.sendFile(path.join(__dirname, 'views','register.html')));
 
+app.get('/login',(req,res) => res.sendFile(path.join(__dirname,'views','login.html')));
 
 
 app.listen(port,()=> console.log('Servidor corriendo en el puerto ' + port))
